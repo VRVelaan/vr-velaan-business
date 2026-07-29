efc5bf65c9c0fffcc1ca806eaf312b647c5f7ec8
VRVelaan <vrvelaan@gmail.com>
2026-07-29 09:49:27 +0530

    Refactor calculate function for improved readability

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-function calculate(){
+function calculate() {
 
-let tonRate = parseFloat(document.getElementById("tonRate").value);
-let weight = parseFloat(document.getElementById("weight").value);
+    let tonRate = Number(document.getElementById("tonRate").value);
+    let weight = Number(document.getElementById("weight").value);
 
-if(isNaN(tonRate) || isNaN(weight)){
-alert("Please enter all values");
-return;
-}
+    if (!tonRate || !weight) {
+        alert("Please enter all values");
+        return;
+    }
 
-let kgRate = tonRate / 1000;
+    let kgRate = tonRate / 1000;
+    let pieceRate = kgRate * weight / 1000;
 
-let pieceRate = kgRate * (weight / 1000);
-
-document.getElementById("kgRate").innerHTML =
-"₹ " + kgRate.toFixed(2);
-
-document.getElementById("pieceRate").innerHTML =
-"₹ " + pieceRate.toFixed(2);
-
+    document.getElementById("kgRate").innerHTML = "₹" + kgRate.toFixed(2);
+    document.getElementById("pieceRate").innerHTML = "₹" + pieceRate.toFixed(2);
 }
